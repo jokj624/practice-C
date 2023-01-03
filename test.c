8ed4bf141cac292fd44a55e3034e61003193d0ba
jokj624 <jokj624@gmail.com>
2023-01-03 14:51:42 +0900

    Feat: week1 실습

---
+++ test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    // printf("Hello World!\n");
+    // printf("Hello\nWorld!\n");
+
+    int height, width, length;
+    float profit;
+    height = 8;
+    width = 9;
+
+    printf("Height: %d\n", height);
+
+    profit = 21.32f;
+    printf("Profit = %f\n", profit);
+    printf("Profit = %.2f\n", profit);
+    
+    length = width + height;
+    printf("length = %d\n", length);
+
+    int a;
+
+    printf("%d\n", a);
+
+    int i;
+    scanf("%d", &i);
+
+    printf("i = %d\n", i);
+    return 0;
+}
